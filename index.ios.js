c4bc9f05998f9a4c1c16e261330210869138df76
Philip Alekseev <phil.alekseev@gmail.com>
2016-03-26 00:41:11 -0400

    start from scratch

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
-import React, {
+var React = require('react-native');
+var {
   AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  MapView,
+  View,
+  StyleSheet
+} = React
 
-class weather extends Component {
-  render() {
+
+var Weather = React.createClass({
+  render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <MapView
+        style={styles.map}>
+      </MapView>
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
-AppRegistry.registerComponent('weather', () => weather);
+var styles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('weather', () => Weather);
